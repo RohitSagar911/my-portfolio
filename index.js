2da159b551cfc9b81ce0834685e79d8e56418ac4
Rohit Sagar <rohitsagarhack@gmail.com>
2025-04-25 11:47:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-function animateSectionItems(sectionSelector, itemSelector, animationClass) {
-  const section = document.querySelector(sectionSelector);
-  const items = section.querySelectorAll(itemSelector);
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
 
-  items.forEach((item) => {
-    item.classList.add(animationClass);
-  });
-}
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Get current section ID
+        const id = entry.target.getAttribute("id");
+
+        // Remove 'active' class from all nav links
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${id}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.6, // Adjust how much of section needs to be visible
+  }
+);
+
+// Observe each section
+sections.forEach((section) => {
+  observer.observe(section);
+});
